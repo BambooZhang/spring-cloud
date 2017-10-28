2ae9bd053dd907253853c3c310b1468abc9bc168
Bamboo <zjcjava@163.com>
2017-10-28 17:27:31 +0800

    第8篇:SpringCloud第八篇-Spring Cloud Config集群

---
+++ chapter8/config-eureka-server/src/main/java/com/bamboo/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.bamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by xialeme on 2017/10/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
